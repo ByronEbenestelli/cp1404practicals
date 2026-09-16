4f4030f02bceb87a933479d66eecc1cc89124e40
Byron Ebenestelli <bebenestelli1@gmail.com>
2026-09-16 12:12:21 +1000

    Refactor password check program with comments

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,14 +1,20 @@
+"""Creates a user password"""
+
+
 def main():
+    """Takes user input for minimum password length and validates password to fit this requirement"""
     minimum_password_length = set_minimum_password_length()
     password = get_valid_password(minimum_password_length)
     print_hidden_password(password)
 
 
 def set_minimum_password_length():
+    """Sets minimum password length"""
     return int(input("Minimum Password Length: "))
 
 
 def get_valid_password(minimum_password_length):
+    """Gets valid password"""
     password = input("Create Password: ")
     while len(password) < minimum_password_length:
         print("Invalid Password")
@@ -17,6 +23,7 @@ def get_valid_password(minimum_password_length):
 
 
 def print_hidden_password(password):
+    """Prints *'s equal to length of password"""
     print("*" * len(password), end="")
 
 
